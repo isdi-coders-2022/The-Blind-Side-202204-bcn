7bc6a188e8948b7b7de831717bc4306e1b43958f
Tomas Sanchez <tms90@outlook.es>
2022-05-02 19:57:11 +0200

    deleted HeaderComponent tag from App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React Router!</h1>
-      <HeaderComponent />
 
       <Routes>
         <Route path="/" element={""} />
