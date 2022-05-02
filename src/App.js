81655c27f237b05fdcab61244a679783d5ff0efa
Marian <marianlopig@gmail.com>
2022-05-02 23:02:28 +0200

    characterDetailComponent created with plain html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import CharacterDetailComponent from "./components/CharacterDetailComponent/CharacterDetailComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React Router!</h1>
+      <CharacterDetailComponent />
       <Routes>
         <Route path="/" element={""} />
       </Routes>
