e8fe8d2e42d942435a797b6c5e3f5c8059f1af91
Marian <marianlopig@gmail.com>
2022-05-03 23:43:35 +0200

    fixed paths for routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Footer from "./components/FooterComponent/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 import HomeComponent from "./pages/HomeComponent/HomeComponent";
@@ -9,7 +9,8 @@ function App() {
       <HeaderComponent />
       <h1>Welcome to React Router!</h1>
       <Routes>
-        <Route path="/" element={HomeComponent} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomeComponent />} />
       </Routes>
       <Footer />
     </div>
