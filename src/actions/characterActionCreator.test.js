5d830fc67aa4bb6939ec77c045e175f9ccc55746
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-04 11:43:26 +0200

    corrected folder route for actions

--- src/actions/characterActionCreator.test.js
+++ src/actions/characterActionCreator.test.js
@@ -2,7 +2,7 @@ import { charactersLoader } from "./characterActionCreator";
 import { characterLoad } from "./characterActionType";
 
 describe("Given characterActionCreator function", () => {
-  describe("When it receives 13 )", () => {
+  describe("When it receives 13", () => {
     test("Then it should return an object with a property type and payLoad 13", () => {
       const character = 13;
 
