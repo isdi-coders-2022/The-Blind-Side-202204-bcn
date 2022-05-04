6529f7dba8455bd2961d567fe8888271dea802f1
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-04 12:35:43 +0200

    implemented characterAdder action

--- src/actions/characterActionCreator.js
+++ src/actions/characterActionCreator.js
@@ -1,6 +1,11 @@
-import { characterLoad } from "./characterActionType";
+import { characterAdd, characterLoad } from "./characterActionType";
 
 export const charactersLoader = (payLoad) => ({
   type: characterLoad,
   payLoad,
+});
+
+export const characterAdder = (character) => ({
+  type: characterAdd,
+  character: character,
 });
