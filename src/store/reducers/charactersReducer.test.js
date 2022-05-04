2b242608ab46a2f2da8a7054b3ee672f7966d496
Cristian Bermudez <ccbabcn@gmail.com>
2022-05-05 01:55:06 +0200

    implemented cahracterLoad case test for characterReducer

--- src/store/reducers/charactersReducer.test.js
+++ src/store/reducers/charactersReducer.test.js
@@ -1,3 +1,4 @@
+import { characterLoad } from "../actions/characterActionType";
 import charactersReducer from "./charactersReducer";
 
 describe("Given the chractersReducer function", () => {
@@ -21,12 +22,47 @@ describe("Given the chractersReducer function", () => {
         species: "Human",
       };
 
-      const newCharacterLoaded = charactersReducer(
+      const newCharacterbyDefault = charactersReducer(
         currentCharacter,
         actionType
       );
 
-      expect(newCharacterLoaded).toEqual(expectedObjectResult);
+      expect(newCharacterbyDefault).toEqual(expectedObjectResult);
+    });
+  });
+
+  describe("When it receives two characters and action type characterLoad", () => {
+    test("Then it should return an object including the given characters", () => {
+      const currentCharacters = [
+        {
+          id: 1,
+          name: "Rick Sanchez",
+          status: "Alive",
+          species: "Human",
+        },
+        { id: 2, name: "Morty Smith", status: "Alive", species: "Human" },
+      ];
+
+      const actionType = {
+        type: characterLoad,
+      };
+
+      const expectedObjectResult = [
+        {
+          id: 1,
+          name: "Rick Sanchez",
+          status: "Alive",
+          species: "Human",
+        },
+        { id: 2, name: "Morty Smith", status: "Alive", species: "Human" },
+      ];
+
+      const newCharactersLoaded = charactersReducer(
+        currentCharacters,
+        actionType
+      );
+
+      expect(newCharactersLoaded).toEqual(expectedObjectResult);
     });
   });
 });
