e4fad8a372f5cc573f558be82a000eb54a3a089e
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-04 23:47:43 +0200

    deleted unused spinner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Footer from "./components/FooterComponent/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
-import PagerComponent from "./components/PagerComponent/PagerComponent";
 import HomeComponent from "./pages/HomeComponent/HomeComponent";
 
 function App() {
