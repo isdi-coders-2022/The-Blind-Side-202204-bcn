34db7a1554ecbe16272507486f8e1f3b2e907be3
Tomas Sanchez <tms90@outlook.es>
2022-05-04 11:26:15 +0200

    added characterActionCreator file and tested

---
+++ store/actions/characterActionCreator.test.js
@@ -0,0 +1,16 @@
+import { charactersLoader } from "./characterActionCreator";
+import { characterLoad } from "./characterActionType";
+
+describe("Given characterActionCreator function", () => {
+  describe("When it receives 13 )", () => {
+    test("Then it should return an object with a property type and payLoad 13", () => {
+      const character = 13;
+
+      const expectObject = { type: characterLoad, payLoad: 13 };
+
+      const result = charactersLoader(character);
+
+      expect(result).toEqual(expectObject);
+    });
+  });
+});
