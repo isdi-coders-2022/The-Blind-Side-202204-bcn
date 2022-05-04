d922042ab6cde7d915e4ec843cf827366f62e329
Marian <marianlopig@gmail.com>
2022-05-05 01:50:38 +0200

    add usecallback in the custom hook to prevent the infinite loop

--- src/components/CharactersListComponent/CharactersListComponent.js
+++ src/components/CharactersListComponent/CharactersListComponent.js
@@ -10,7 +10,7 @@ const CharactersListComponent = () => {
 
   useEffect(() => {
     loadCharacters();
-  }, []);
+  }, [loadCharacters]);
 
   return (
     <CharacterList>
--- src/store/hooks/useApi.js
+++ src/store/hooks/useApi.js
@@ -1,21 +1,24 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { charactersLoader } from "../actions/characterActionCreator";
 import CharacterContext from "../contexts/CharacterContext";
 
 const useApi = () => {
   const { dispatch } = useContext(CharacterContext);
 
-  const loadCharacters = (url) => {
-    if (url === undefined) {
-      url = "https://rickandmortyapi.com/api/character/?page=1";
-    }
+  const loadCharacters = useCallback(
+    (url) => {
+      if (url === undefined) {
+        url = "https://rickandmortyapi.com/api/character/?page=1";
+      }
 
-    (async () => {
-      const response = await fetch(url);
-      const characterListData = await response.json();
-      dispatch(charactersLoader(characterListData));
-    })();
-  };
+      (async () => {
+        const response = await fetch(url);
+        const characterListData = await response.json();
+        dispatch(charactersLoader(characterListData));
+      })();
+    },
+    [dispatch]
+  );
 
   return { loadCharacters };
 };
