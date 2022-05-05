b1ed9c41644d6a301ccaddb9c884f477b01bc200
Tomas Sanchez <tms90@outlook.es>
2022-05-05 17:30:13 +0200

    added favoriteComponent file

--- src/pages/FavoriteComponent/FavoriteComponent.js
+++ src/pages/FavoriteComponent/FavoriteComponent.js
@@ -1,6 +1,15 @@
 import CharactersListComponent from "../../components/CharactersListComponent/CharactersListComponent";
+import { useContext, useEffect } from "react";
+import useApi from "../../store/hooks/useApi";
+import CharacterContext from "../../store/contexts/CharacterContext";
 
 const FavoriteComponent = () => {
+  const { state } = useContext(CharacterContext);
+  const { loadCharacters } = useApi();
+
+  useEffect(() => {
+    loadCharacters();
+  }, [loadCharacters]);
   return (
     <>
       <CharactersListComponent />
