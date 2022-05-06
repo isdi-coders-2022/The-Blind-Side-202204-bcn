19bbd8d0ed9b609db2f2af7a202c043eb159e76c
Marian <marianlopig@gmail.com>
2022-05-06 03:58:29 +0200

    card detail css

--- src/components/CharacterDetailComponent/CharacterDetailComponent.js
+++ src/components/CharacterDetailComponent/CharacterDetailComponent.js
@@ -1,27 +1,33 @@
 import { MdRefresh } from "react-icons/md";
 import { FaHeart } from "react-icons/fa";
+import { CharacterDetailStyles } from "./CharacterDetailComponentStyle";
 
-const CharacterDetailComponent = ({
-  character: { name, status, species, gender, image },
-}) => {
+const CharacterDetailComponent = ({ character }) => {
+  const { name, status, species, image, gender } = character;
   return (
-    <>
-      <div>
-        <img src={image} alt={name} />
-        <div>
-          <h2>{name}</h2>
-          <ul>
-            <li>Species: {species}</li>
-            <li>Status: {status}</li>
-            <li>Gender: {gender}</li>
-          </ul>
+    <CharacterDetailStyles>
+      <div className="main-container">
+        <div className="character">
+          <div className="character__image-container">
+            <img className="character__image" src={image} alt={name} />
+          </div>
           <div>
-            <MdRefresh />
-            <FaHeart />
+            <h2>{name}</h2>
+            <ul>
+              <li>Species: {species}</li>
+              <li>Status: {status}</li>
+              <li>Gender: {gender}</li>
+            </ul>
+            <div className="icon-container">
+              <div className="character__icons">
+                <MdRefresh className="icon-container icon-container--info" />
+                <FaHeart className="icon-container icon-container--favourite" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </CharacterDetailStyles>
   );
 };
 
